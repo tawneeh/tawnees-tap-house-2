37bfef4c831d6bd48175095f1b9094a82155c4b7
Tawnee Harris <tawneeh@icloud.com>
2021-02-19 12:12:41 -0800

    add action tests to root reducer file

--- src/__tests__/reducers/index-reducer.test.js
+++ src/__tests__/reducers/index-reducer.test.js
@@ -2,6 +2,7 @@ import rootReducer from '../../reducers/index';
 import { createStore } from 'redux';
 import kegListReducer from '../../reducers/keg-list-reducer';
 import formVisibleReducer from '../../reducers/form-visible-reducer';
+import * as c from '../../actions/ActionTypes';
 
 let store = createStore(rootReducer);
 
@@ -20,6 +21,28 @@ describe("rootReducer", () => {
 
   test('Check that initial state of formVisibleReducer matches root reducer', () => {
     expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, { type: null }));
+  });
+
+  test('Check that ADD_KEG action works for kegListReducer and root reducer', () => {
+    const action = {
+      type: c.ADD_KEG,
+      name: '10 Below',
+      brand: 'Scuttlebutt Brewing',
+      price: '5',
+      alcoholContent: '7',
+      pintsLeft: '124',
+      id: 1
+    }
+    store.dispatch(action);
+    expect(store.getState().masterKegList).toEqual(kegListReducer(undefined, action));
+  });
+
+  test('Check that TOGGLE_FORM works for formVisibleReducer and root reducer', () => {
+    const action = {
+      type: c.TOGGLE_FORM
+    }
+    store.dispatch(action);
+    expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, action));
   });
 
 });
