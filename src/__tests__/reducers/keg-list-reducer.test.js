cc99e0a402377d4ba00aa52868399f4a10d52771
Tawnee Harris <tawneeh@icloud.com>
2021-02-19 08:31:43 -0800

    add statement to import Action constants

--- src/__tests__/reducers/keg-list-reducer.test.js
+++ src/__tests__/reducers/keg-list-reducer.test.js
@@ -1,4 +1,5 @@
 import kegListReducer from '../../reducers/keg-list-reducer';
+import * as c from '../../actions/ActionTypes';
 
 describe('kegListReducer', () => {
   test('Should return default state if there is no action type passed in', () => {
