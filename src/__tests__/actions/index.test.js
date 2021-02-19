8903104f4a68418907bb09edede8ab62655ce6e0
Tawnee Harris <tawneeh@icloud.com>
2021-02-19 13:45:28 -0800

    add a test for the addKeg action

--- src/__tests__/actions/index.test.js
+++ src/__tests__/actions/index.test.js
@@ -1,10 +1,24 @@
 import * as actions from './../../actions';
+import * as c from '../../actions/ActionTypes';
 
 describe('tap house actions', () => {
 
   it('toggleForm should create TOGGLE_FORM action', () => {
     expect(actions.toggleForm()).toEqual({
-      type: 'TOGGLE_FORM'
+      type: c.TOGGLE_FORM
     });
   });
+
+  it('addKeg should create ADD_KEG action', () => {
+    expect(actions.addKeg({name: '10 Below', brand: 'Scuttlebutt Brewing', price: '5', alcoholContent: '7', pintsLeft: '124', id: 1})).toEqual({
+    type: c.AFF_KEG,
+    name: '10 Below',
+    brand: 'Scuttlebutt Brewing',
+    price: '5',
+    alcoholContent: '7',
+    pintsLeft: '124',
+    id: 1
+    });
+  });
+
 });
