aa4d9012e16f22cab54567ecac30f883ae06b338
Tawnee Harris <tawneeh@icloud.com>
2021-02-19 10:54:48 -0800

    update KegList as an object

--- src/components/KegList.js
+++ src/components/KegList.js
@@ -5,7 +5,8 @@ import PropTypes from "prop-types";
 function KegList(props) {
   return (
     <>
-      {props.kegList.map((keg) =>
+      <hr />
+      {Object.values(props.kegList).map((keg) =>
         <Keg
           whenKegClicked = { props.onKegSelection }
           name={keg.name}
@@ -20,7 +21,7 @@ function KegList(props) {
 }
 
 KegList.propTypes = {
-  kegList: PropTypes.array,
+  kegList: PropTypes.object,
   onKegSelection: PropTypes.func
 };
 
