fc712b58ac799e0ab555660244050bde755017bb
Tawnee Harris <tawneeh@icloud.com>
2021-02-19 12:02:57 -0800

    add smoke tests

--- src/__tests__/reducers/index-reducer.test.js
+++ src/__tests__/reducers/index-reducer.test.js
@@ -1,4 +1,9 @@
 import rootReducer from '../../reducers/index';
+import { createStore } from 'redux';
+import kegListReducer from '../../reducers/keg-list-reducer';
+import formVisibleReducer from '../../reducers/form-visible-reducer';
+
+let store = createStore(rootReducer);
 
 describe("rootReducer", () => {
 
@@ -7,6 +12,14 @@ describe("rootReducer", () => {
       masterKegList: {},
       formVisibleOnPage: false
     });
+  });
+
+  test('Check that initial state of kegListReducer matches the root reducer', () => {
+    expect(store.getState().masterKegList).toEqual(kegListReducer(undefined, { type: null }));
+  });
+
+  test('Check that initial state of formVisibleReducer matches root reducer', () => {
+    expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, { type: null }));
   });
 
 });
