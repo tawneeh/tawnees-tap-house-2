b8b42095e40c71414c5deaf30de465294e036f12
Tawnee Harris <tawneeh@icloud.com>
2021-02-19 11:42:53 -0800

    add test to toggle the form

--- src/__tests__/reducers/form-visible-reducer.test.js
+++ src/__tests__/reducers/form-visible-reducer.test.js
@@ -1,8 +1,13 @@
 import formVisibleReducer from '../../reducers/form-visible-reducer';
+import * as c from '../../actions/ActionTypes';
 
 describe("formVisibleReducer", () => {
 
   test('Should return default state if there is no action type passed in', () => {
     expect(formVisibleReducer(false, { type: null })).toEqual(false);
+  });
+
+  test('Should toggle form visibility state to true', () => {
+    expect(formVisibleReducer(false, { type: c.TOGGLE_FORM })).toEqual(true);
   });
 });
--- src/reducers/form-visible-reducer.js
+++ src/reducers/form-visible-reducer.js
@@ -1,3 +1,9 @@
+import * as c from './../actions/ActionTypes';
+
 export default (state = false, action) => {
+  switch (action.type) {
+  case c.TOGGLE_FORM:
+    return !state;
+  }
   return state;
 };
