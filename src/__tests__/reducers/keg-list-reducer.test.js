7717d37b1641831b0370091beec24c9e769b5cad
Tawnee Harris <tawneeh@icloud.com>
2021-02-19 08:24:03 -0800

    add test for default state

---
+++ src/__tests__/reducers/keg-list-reducer.test.js
@@ -0,0 +1,7 @@
+import kegListReducer from '../../reducers/keg-list-reducer';
+
+describe('kegListReducer', () => {
+  test('Should return default state if there is no action type passed in', () => {
+    expect(kegListReducer({}, { type: null })).toEqual({});
+  });
+});
